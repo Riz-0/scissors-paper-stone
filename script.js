f2d658d5a5cd4621a9b0f7b0f36365ebf113d0d5
Riz <55377396+Riz-0@users.noreply.github.com>
2025-03-13 14:59:12 +0800

    Add playRound()

--- script.js
+++ script.js
@@ -22,7 +22,40 @@ function getHumanChoice() {
     return choice;
 }
 
+function playRound (humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == computerChoice) {
+        // If human and computer pick the same, it's a tie
+        return "tie";
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        // If human chooses scissors and computer chooses paper, human wins
+        return "human"
+    } else if (humanChoice == "scissors" && computerChoice == "stone") {
+        // If human chooses scissors and computer chooses stone, computer wins
+        return "computer"
+    } else if (humanChoice = "paper" && computerChoice == "stone") {
+        // If human chooses paper and computer chooses stone, human wins
+        return "human";
+    }    else if (humanChoice = "paper" && computerChoice == "scissors") {
+        // If human chooses paper and computer chooses scissors, computer wins
+        return "computer";
+    }   else if (humanChoice = "stone" && computerChoice == "scissors") {
+        // If human chooses stone and computer chooses scissors, human wins
+        return "human";
+    }    else if (humanChoice == "stone" && computerChoice == "paper") {
+        // If human chooses stone and computer chooses paper, computer wins
+        return "computer";
+    }
+}
+
 function main() {
     let humanScore = 0;
     let computerScore = 0;
+    let winner = playRound();
+
+    if (winner == "human") {
+        humanScore += 1;
+    } else if (winner == "computer") {
+        computerScore += 1;
+    }
 }
