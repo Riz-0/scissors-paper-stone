c938a269e6c53c82e8cd4b99837e336f1588830f
Riz <55377396+Riz-0@users.noreply.github.com>
2025-03-13 15:01:49 +0800

    Add win-lose message

--- script.js
+++ script.js
@@ -26,24 +26,31 @@ function playRound (humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
     if (humanChoice == computerChoice) {
         // If human and computer pick the same, it's a tie
+        console.log("It's a tie!")
         return "tie";
     } else if (humanChoice == "scissors" && computerChoice == "paper") {
         // If human chooses scissors and computer chooses paper, human wins
+        console.log("You Win! Scissors beats Paper!");
         return "human"
     } else if (humanChoice == "scissors" && computerChoice == "stone") {
         // If human chooses scissors and computer chooses stone, computer wins
+        console.log("You Lose! Stone beats Scissors!");
         return "computer"
     } else if (humanChoice = "paper" && computerChoice == "stone") {
         // If human chooses paper and computer chooses stone, human wins
+        console.log("You Win! Paper beats Stone!");
         return "human";
     }    else if (humanChoice = "paper" && computerChoice == "scissors") {
         // If human chooses paper and computer chooses scissors, computer wins
+        console.log("You Lose! Scissors beats Paper!");
         return "computer";
     }   else if (humanChoice = "stone" && computerChoice == "scissors") {
         // If human chooses stone and computer chooses scissors, human wins
+        console.log("You Win! Stone beats Scissors!");
         return "human";
     }    else if (humanChoice == "stone" && computerChoice == "paper") {
         // If human chooses stone and computer chooses paper, computer wins
+        console.log("You Lose! Stone beats Paper!");
         return "computer";
     }
 }
