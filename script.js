63f623bebace75537d9a5188d189d1d7708ffb43
Riz <55377396+Riz-0@users.noreply.github.com>
2025-03-19 14:34:40 +0800

    Simplify playRound() function

--- script.js
+++ script.js
@@ -13,35 +13,19 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
-  if (humanChoice == computerChoice) {
-    // If human and computer pick the same, it's a tie
-    console.log("It's a tie!");
-    return "tie";
-  } else if (humanChoice == "scissors" && computerChoice == "paper") {
-    // If human chooses scissors and computer chooses paper, human wins
-    console.log("You Win! Scissors beats Paper!");
-    return "human";
-  } else if (humanChoice == "scissors" && computerChoice == "stone") {
-    // If human chooses scissors and computer chooses stone, computer wins
-    console.log("You Lose! Stone beats Scissors!");
-    return "computer";
-  } else if ((humanChoice = "paper" && computerChoice == "stone")) {
-    // If human chooses paper and computer chooses stone, human wins
-    console.log("You Win! Paper beats Stone!");
-    return "human";
-  } else if ((humanChoice = "paper" && computerChoice == "scissors")) {
-    // If human chooses paper and computer chooses scissors, computer wins
-    console.log("You Lose! Scissors beats Paper!");
-    return "computer";
-  } else if ((humanChoice = "stone" && computerChoice == "scissors")) {
-    // If human chooses stone and computer chooses scissors, human wins
-    console.log("You Win! Stone beats Scissors!");
-    return "human";
-  } else if (humanChoice == "stone" && computerChoice == "paper") {
-    // If human chooses stone and computer chooses paper, computer wins
-    console.log("You Lose! Stone beats Paper!");
-    return "computer";
+  switch (true) {
+    case humanChoice == computerChoice:
+      return "tie";
+    case humanChoice == "scissors" && computerChoice == "paper":
+    case humanChoice == "paper" && computerChoice == "stone":
+    case humanChoice == "stone" && computerChoice == "scissors":
+      return "human";
+    case computerChoice == "scissors" && humanChoice == "paper":
+    case computerChoice == "paper" && humanChoice == "stone":
+    case computerChoice == "stone" && humanChoice == "scissors":
+      return "computer";
+    default:
+      console.log("ERROR: No winner");
   }
 }
 
